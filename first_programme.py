ed5d11a100aebda07c9597d6efefb1cbe4c40a6c
Siva Sankar <sivasankarbommisetty@gmail.com>
2026-09-03 20:40:15 +0530

    Practise python code

---
+++ first_programme.py
@@ -0,0 +1,13 @@
+n=1257
+rev =0
+while n>0:
+    r=n%10
+    rev=rev*10 +r
+    n=n//10
+print('reverse number is :',rev);
+
+print('hello world')
+
+s1 = 'hello'
+print(type(s1))
+print('hello\fworld')
